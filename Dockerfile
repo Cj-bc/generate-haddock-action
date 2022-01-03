41227baff865b67eba6076d6bd0972aa9dbe0cce
Cj-bc <cj.bc-sd@outlook.jp>
2022-01-03 18:00:54 +0900

    Change: this action won't try to commit documents

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,4 @@
 FROM haskell:8.10
 
-RUN git worktree add haddock haddock
-
 RUN stack build --only-dependencies
-
 RUN stack haddock --haddock-arguments --odir=haddock
-
-workdir haddock
-CMD ./commit-artifact.sh
--- commit-artifact.sh
+++
@@ -1,9 +0,0 @@
-HADDOCK_BRANCH=haddock
-git switch ${HADDOCK_BRANCH}
-
-git add *
-
-git config user.name github-actions
-git config user.email github-actions@github.com
-git commit -m "Github action: Auto commit"
-git push origin ${HADDOCK_BRANCH}
